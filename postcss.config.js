26c1722a16919c3f014da43f0b6faf5bc1b75902
Rishab Garg <rishgarg@gmail.com>
2022-07-13 19:43:57 +0200

    style: formatted files with prettier

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,4 @@
-const tailwindcss = require('tailwindcss');
+const tailwindcss = require("tailwindcss")
 module.exports = {
-  plugins: [
-    'postcss-preset-env',
-    tailwindcss,
-    'autoprefixer'
-  ],
-};
+  plugins: ["postcss-preset-env", tailwindcss, "autoprefixer"],
+}
